f2f04a3fae81a8a5bd7666ff2f11154b20046fcc
brunobertholdi <bruno.bertholdi@madeinweb.com.br>
2025-01-24 23:32:05 -0300

    chore: initial workflow with logfire tracing :)

---
+++ main.py
@@ -0,0 +1,32 @@
+# %%
+# --- Custom modules for the project --- #
+from agents import generic_openai_agent as llm
+from structured_output import output_classes as output
+from pdf_parsing import pdf_parser
+
+# --- Python modules --- #
+import logfire # for debugging, logging and tracing results
+import toml # for prompts
+from pydantic_ai import Agent # AI models and agents
+
+# --- Load the prompts --- #
+prompts = toml.load("prompts/prompts.toml")
+
+# %%
+# --- Configure logfire --- #
+logfire.configure()
+
+# --- Build agents --- #
+field_extraction_agent = Agent(model=llm.gpt_4o, result_type=output.DocumentStructure)
+
+# %%
+# --- Load the pdf file --- #
+text = pdf_parser.extract_text_from_pdf("data/monark.pdf")
+
+# %%
+ppt = prompts["field_extraction"]["field_extraction"].format(text=text)
+field_extraction = field_extraction_agent.run_sync(ppt)
+
+logfire.notice("Field extraction result: {field_extraction}", field_extraction=dict(field_extraction.data))
+logfire.info("Result type: {field_extraction}", field_extraction=type(field_extraction.data))
+# %%
